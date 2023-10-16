f0172f4a67824cf77f778ee59c9764192fd5482f
goknurbilisim1 <145705901+goknurbilisim1@users.noreply.github.com>
2023-10-16 14:33:33 +0300

    Add files via upload

---
+++ comboBox-if-ornek/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace comboBox_if_ornek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.cmbGun = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.Location = new System.Drawing.Point(44, 79);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(57, 20);
+            this.lblSonuc.TabIndex = 0;
+            this.lblSonuc.Text = "label1";
+            // 
+            // cmbGun
+            // 
+            this.cmbGun.FormattingEnabled = true;
+            this.cmbGun.Items.AddRange(new object[] {
+            "Pazartesi",
+            "Salı",
+            "Çarşamba",
+            "Perşembe",
+            "Cuma",
+            "Cumartesi",
+            "Pazar"});
+            this.cmbGun.Location = new System.Drawing.Point(37, 35);
+            this.cmbGun.Name = "cmbGun";
+            this.cmbGun.Size = new System.Drawing.Size(251, 21);
+            this.cmbGun.TabIndex = 1;
+            this.cmbGun.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 247);
+            this.Controls.Add(this.cmbGun);
+            this.Controls.Add(this.lblSonuc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.ComboBox cmbGun;
+    }
+}
+
---
+++ comboBox-if-ornek/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace comboBox_if_ornek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string gunad=cmbGun.Text;
+             int gunNo=cmbGun.SelectedIndex;
+            gunNo++;
+            lblSonuc.Text = gunad + "haftanın" + gunNo + "günüdür";
+
+            if (gunNo==1|| gunNo==2|| gunNo==3)
+            {
+                lblSonuc.Text = "Çıkış Saatleri 16:40";
+            }
+            else if (gunNo == 4 || gunNo == 5)
+            {
+                lblSonuc.Text= "Çıkış Saatleri 15:50";
+            }
+            else
+            {
+                lblSonuc.Text= "TATİL";
+            }
+                
+            
+            
+        }
+    }
+}
